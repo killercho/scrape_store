bf84f757dff4a62daee8f7118492f70af80204f3
Georgi Nikolov <ggeorgi60@gmail.com>
2023-12-28 17:17:52 +0200

    Fixed server flake8 style

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 '''
 Final form of the server part for the server-client web scraper application.
-In this form after the server establishes connection and receives the information from the client
-the server is expected to run the web scraper and return the appropriate prices for the
-requested game.
-The search is all connected to the steam search. It will return the value for the game
-that is closest to the user search. In the odd case that no such games are found the server
+In this form after the server establishes connection and receives the
+information from the client the server is expected to run the web scraper
+and return the appropriate prices for the requested game.
+The search is all connected to the steam search.
+It will return the value for the game that is closest to the user search.
+In the odd case that no such games are found the server
 returns [-1, -1] to indicate that the client made a mistake.
 If the searched game is actually "Free to play" then the server returns [0, 0].
 '''
@@ -19,6 +20,7 @@ from bs4 import BeautifulSoup, ResultSet
 # Constants
 TRANSFER_SIZE = 1024
 URL = 'https://store.steampowered.com/search/?term='
+
 
 def handle_connection(conn) -> None:
     '''
@@ -52,16 +54,16 @@ def handle_connection(conn) -> None:
             game_name_found = game_names_found[0].get_text()
 
             # Grab all divs that correspond to a value of the game
-            combined_prices_div = soup.find_all("div", class_="discount_prices")[0]
-            all_divs_arr = combined_prices_div.find_all("div")
+            combined_prices = soup.find_all("div", class_="discount_prices")[0]
+            all_divs_arr = combined_prices.find_all("div")
 
             if len(all_divs_arr) == 2:
                 # If the game has two divs with values then it is discounted
-                original_value: str= all_divs_arr[0].get_text()
+                original_value: str = all_divs_arr[0].get_text()
                 discounted_value: str = all_divs_arr[1].get_text()
                 response_value = [original_value, discounted_value]
             elif len(all_divs_arr) == 1:
-                # If the game has one div then it is either free or not discounted
+                # If the game has one div then is either free or not discounted
                 value = all_divs_arr[0].get_text()
                 if value == "Free":
                     response_value = ["0", "0"]
@@ -72,7 +74,6 @@ def handle_connection(conn) -> None:
         # Send the data back to the client with pickle to encode it
         conn.send(pickle.dumps([game_name_found, response_value]))
     conn.close()
-
 
 def main() -> None:
     '''
@@ -92,7 +93,7 @@ def main() -> None:
     while True:
         conn = None
         try:
-            # Create a connection when a client requests it on a different thread
+            # Create a connection when a client requests on a different thread
             conn, address = sock.accept()
             print('Connected to address: ', address[0], ':', address[1])
             start_new_thread(handle_connection, (conn,))
